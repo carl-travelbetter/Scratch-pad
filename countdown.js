0056e045313138a2dcd62ce6c3829696646a15a0
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-11-22 19:40:04 +0000

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,7 +1,13 @@
 let today = new Date();
 let targetDate = new Date("2025, 09, 24, 18:00:00");
 
-function getCountdown()
+function createCountdown()
 {
+  console.log("Create Countdown");
+  //create a basic countdown and get the maths right.
+  const date = document.getElementById("date").valueAsDate;
+  let today = new Date();
+  let secondsBetween = date - today;
+  let daysBetween = Math.floor(secondsBetween / (1000 * 60 * 60 *24));
   
 }
