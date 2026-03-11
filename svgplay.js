17eced11ab8f764f22adfd8af8599a0008f83a9d
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-03-11 21:35:24 +0000

    Update svgplay.js

--- svgplay.js
+++ svgplay.js
@@ -8,5 +8,5 @@ circle.addEventListener("click", () => {
       console.log("Circle Expense Button Clicked");
       radius += 2;
       circle.setAttribute("fill", "gold");
-      console.log('Radius is now '+radius);
+      console.log('Radius is now fill '+radius);
       });
